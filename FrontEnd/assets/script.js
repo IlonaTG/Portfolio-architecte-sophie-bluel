7db5e8e3c4a3335178627b6c4abf50c80c112c2c
Ilona <ilona.tsivunchyk@gmail.com>
2023-11-29 17:15:23 +0100

    filters done

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -27,27 +27,82 @@ fetch("http://localhost:5678/api/works/")
 const itemsContainer = document.querySelector(".gallery");
 
 // Fonction pour ajouter les travaux à la galerie
-async function ajouterTravauxALaGalerie() {
+async function afficherToutesLesImages() {
   itemsContainer.innerHTML = "";
 
+  // Une requête Fetch pour obtenir les données de l'API
   const response = await fetch("http://localhost:5678/api/works/")
     .then((response) => response.json())
     .then((data) => {
-  data.forEach(element => {
-    const figure = document.createElement("figure");
-    // Créer une image pour le projet
-    const image = document.createElement("img");
-    image.src = element.imageUrl;
+      data.forEach(element => {
+        const figure = document.createElement("figure");
+        // Créer une image pour le projet
+        const image = document.createElement("img");
+        image.src = element.imageUrl;
+        
+        // Créer un nom pour afficher le nom du projet
+        const caption = document.createElement("figcaption");
+        caption.textContent = element.title;
 
-    // Créer un nom pour afficher le nom du projet
-    const caption = document.createElement("figcaption");
-    caption.textContent = element.title;
-
-    // Ajouter l'image et le nom du projet à la div du projet
+        // Ajouter l'image et le nom du projet à la div du projet
    
-    figure.appendChild(image);
-    figure.appendChild(caption);
-    itemsContainer.appendChild(figure);
-  });
-});
+        figure.appendChild(image);
+        figure.appendChild(caption);
+        itemsContainer.appendChild(figure);
+      });
+    });
 }
+
+// Fonction de tri par catégorie
+async function trierParCategorie(categorie) {
+  // Effacer les éléments actuels dans le conteneur
+  itemsContainer.innerHTML = "";
+
+  // Effectuer une requête Fetch pour obtenir les données de l'API
+  const response = await fetch("http://localhost:5678/api/works/")
+    .then((response) => response.json())
+    .then((data) => {
+      // Filtrer les éléments en fonction de la catégorie sélectionnée
+      const elementsFiltres = data.filter(
+        (element) => element.category.name === categorie
+      );
+
+      // Parcourir les éléments filtrés et les ajouter au conteneur
+      elementsFiltres.forEach((element) => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        image.src = element.imageUrl;
+        const caption = document.createElement("figcaption");
+        caption.textContent = element.title;
+
+        figure.appendChild(image);
+        figure.appendChild(caption);
+        itemsContainer.appendChild(figure);
+      });
+    });
+}
+
+// Ajouter des écouteurs d'événements pour les boutons de tri
+const tousBtn = document.createElement("button");
+tousBtn.textContent = "Tous";
+tousBtn.addEventListener("click", afficherToutesLesImages);
+document.querySelector(".filters").appendChild(tousBtn);
+
+const objetsBtn = document.createElement("button");
+objetsBtn.textContent = "Objets";
+objetsBtn.addEventListener("click", () => trierParCategorie("Objets"));
+document.querySelector(".filters").appendChild(objetsBtn);
+
+const appartementsBtn = document.createElement("button");
+appartementsBtn.textContent = "Appartements";
+appartementsBtn.addEventListener("click", () => trierParCategorie("Appartements"));
+document.querySelector(".filters").appendChild(appartementsBtn);
+
+const hotelsBtn = document.createElement("button");
+hotelsBtn.textContent = "Hotels & restaurants";
+hotelsBtn.addEventListener("click", () => trierParCategorie("Hotels & restaurants"));
+document.querySelector(".filters").appendChild(hotelsBtn);
+
+
+
+
