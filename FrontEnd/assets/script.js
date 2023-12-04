a4981148bd0316d95f73650b66db98b1ee28feb0
Ilona <ilona.tsivunchyk@gmail.com>
2023-12-04 16:55:22 +0100

    log in modifications

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -103,6 +103,30 @@ hotelsBtn.textContent = "Hotels & restaurants";
 hotelsBtn.addEventListener("click", () => trierParCategorie("Hotels & restaurants"));
 document.querySelector(".filters").appendChild(hotelsBtn);
 
+//Les changement en mode logIn
+const log = document.querySelector(".log");
+const banner = document.querySelector(".edition-banner");
+const modif = document.querySelector(".mesProjets-changing");
+const container = document.querySelector(".edition-container");
+const filtres = document.querySelector(".filters");
 
+function editionActive() {
+  if (localStorage.login) {
+    (log.innerText = "logout"), (banner.style = "display:flex;");
+    filtres.style = "display:none";
+  } else {
+    banner.style = "display:none;";
+    modif.style = "display:none;";
+    container.style = "display:none";
+  }
+}
+editionActive();
+
+log.addEventListener("click", () => {
+  localStorage.removeItem("login");
+  localStorage.removeItem("token");
+  log.innerText = "login";
+  localStorage.clear;
+});
 
 
